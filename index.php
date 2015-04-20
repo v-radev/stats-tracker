540b7e8e63b4058314d8eb53612631430bf5cf0a
v.radev <mail@radev.info>
2015-04-20 15:36:44 +0300

    First

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title>Test page</title>
+    <script type="text/javascript" src="jquery-1.11.2.js"></script>
+    <script type="text/javascript" src="stats-tracker.js"></script>
+</head>
+<body>
+
+</body>
+</html>
